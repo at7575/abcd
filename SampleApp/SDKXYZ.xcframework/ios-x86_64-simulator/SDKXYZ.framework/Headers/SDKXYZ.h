4d402a2110731e6173ff506452096bae72a223b6
at7575 <test@example.com>
2023-08-07 12:01:00 +0800

    cleanup sampleapp

---
+++ SampleApp/SDKXYZ.xcframework/ios-x86_64-simulator/SDKXYZ.framework/Headers/SDKXYZ.h
@@ -0,0 +1,18 @@
+//
+//  SDKXYZ.h
+//  SDKXYZ
+//
+//  Created by Wong Peter on 4/8/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKXYZ.
+FOUNDATION_EXPORT double SDKXYZVersionNumber;
+
+//! Project version string for SDKXYZ.
+FOUNDATION_EXPORT const unsigned char SDKXYZVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKXYZ/PublicHeader.h>
+
+
